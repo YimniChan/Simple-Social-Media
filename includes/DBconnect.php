f1955566c68cdfb872b4b4ed4955f3fc91a458df
Yimni Chan <ynchan818@gmail.com>
2020-02-13 09:26:03 -0500

    Update DBconnect.php

--- includes/DBconnect.php
+++ includes/DBconnect.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_USER', 'chan');
-define('DB_PASSWORD', 'yimni23503159');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'chan_SocialMedia');
+define('DB_USER', '____');//your username
+define('DB_PASSWORD', '_____');//your password
+define('DB_HOST', '_____');//your database location
+define('DB_NAME', '_____');//your database name
 
 $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) 
 	OR die('Could not connect to MySQL: ' . mysqli_connect_error() );
